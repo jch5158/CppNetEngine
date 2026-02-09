8e1d9b7b040603e41191634e9a80664dbf3dd01b
devhun001 <devhun001@gmail.com>
2026-02-09 17:58:54 +0900

    .

--- CppNetEngine/UniquePtrUtils.h
+++ CppNetEngine/UniquePtrUtils.h
@@ -29,11 +29,7 @@ public:
 		const uint64 memSize = sizeof(T);
 		void* memPtr = MemoryAllocator::GetInstance().Alloc(memSize);
 
-		T* returnPtr = static_cast<T*>(memPtr);
-		if (!std::is_trivially_default_constructible_v<T>)
-		{
-			returnPtr = new(memPtr) T(std::forward<Args>(args)...);
-		}
+		T* returnPtr = new(memPtr) T(std::forward<Args>(args)...);
 
 		return std::unique_ptr<T, Deleter>(returnPtr);
 	}
@@ -41,63 +37,69 @@ public:
 
 // 2. [특수화] 배열용 (Array, 예: char[])
 template <typename T>
-class UniquePtrUtils<T[]>
+class UniquePtrUtils<T[]> final
 {
 public:
-	// 상태가 있는 Deleter (크기를 기억해야 함)
 	class ArrayDeleter
 	{
 	public:
-
-		explicit ArrayDeleter(const uint64 size)
-			:mSize(size)
+		explicit ArrayDeleter(const uint64 size) 
+		: mSize(size)
 		{
 		}
 
 		void operator()(T* ptr) const
 		{
-			// 클래스 배열이면 역순 소멸 (char[] 같은 건 무시됨)
 			if constexpr (std::is_class_v<T>)
 			{
 				const uint64 count = mSize / sizeof(T);
 				for (uint64 i = 0; i < count; ++i)
 				{
-					ptr[count - i - 1].~T();
+					ptr[count - 1 - i].~T();
 				}
 			}
-
 			MemoryAllocator::GetInstance().Free(ptr, mSize);
 		}
 
 	private:
-
-		uint64 mSize; // 할당된 바이트 크기 저장
+		uint64 mSize;
 	};
 
-	// 배열은 생성자 인자 전달이 복잡하므로 보통 개수(count)만 받음
 	static std::unique_ptr<T[], ArrayDeleter> Alloc(const uint64 count)
 	{
 		const uint64 size = sizeof(T) * count;
-		void* ptr = MemoryAllocator::GetInstance().Alloc(size);
+		void* voidPtr = MemoryAllocator::GetInstance().Alloc(size);
 
+		// [수정 2] void*를 T*로 변환해야 인덱싱 가능
+		T* typePtr = static_cast<T*>(voidPtr);
+
+		// 배열은 기본 생성자 호출이므로 Trivial 최적화 가능
 		if constexpr (!std::is_trivially_default_constructible_v<T>)
 		{
 			for (uint64 i = 0; i < count; ++i)
 			{
-				new (&ptr[i]) T();
+				new (&typePtr[i]) T();
 			}
 		}
 
-		// ArrayDeleter에 사이즈를 넣어서 생성
-		return std::unique_ptr<T[], ArrayDeleter>(static_cast<T*>(ptr), ArrayDeleter(size));
+		return std::unique_ptr<T[], ArrayDeleter>(typePtr, ArrayDeleter(size));
 	}
 };
 
+// ==========================================
+// 타입 헬퍼
+// ==========================================
 template <typename T>
-struct GetUniquePtrType { using Type = std::unique_ptr<T, typename UniquePtrUtils<T>::Deleter>; };
+struct GetUniquePtrType
+{
+	using Type = std::unique_ptr<T, typename UniquePtrUtils<T>::Deleter>;
+};
 
 template <typename T>
-struct GetUniquePtrType<T[]> { using Type = std::unique_ptr<T[], typename UniquePtrUtils<T[]>::ArrayDeleter>; };
+struct GetUniquePtrType<T[]>
+{
+	using Type = std::unique_ptr<T[], typename UniquePtrUtils<T[]>::ArrayDeleter>;
+};
 
 template <typename T>
-using UniquePtr = GetUniquePtrType<T>::Type;
+using UniquePtr = GetUniquePtrType<T>::Type; 
