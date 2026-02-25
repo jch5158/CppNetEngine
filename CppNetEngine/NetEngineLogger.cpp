da6383c0d358d7217d58948051da3e5222f39fb2
devhun001 <devhun001@gmail.com>
2026-02-25 21:31:19 +0900

    logger 수정

--- CppNetEngine/NetEngineLogger.cpp
+++ CppNetEngine/NetEngineLogger.cpp
@@ -10,13 +10,13 @@ void NetEngineLogger::Init()
 {
 	spdlog::init_thread_pool(8192, 1);
 
-	std::vector<spdlog::sink_ptr> sinks;
+	Vector<spdlog::sink_ptr> sinks;
 
-	const auto fileSink = std::make_shared<spdlog::sinks::daily_file_sink_mt>("logs/NetEngine.log", 0, 0);
+	const auto fileSink = cpp_net_engine::MakeShared<spdlog::sinks::daily_file_sink_mt>("logs/NetEngine.log", 0, 0);
 	sinks.emplace_back(fileSink);
 
 #ifdef _DEBUG
-	const auto consoleSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+	const auto consoleSink = cpp_net_engine::MakeShared<spdlog::sinks::stdout_color_sink_mt>();
 	sinks.emplace_back(consoleSink);
 #endif
 
@@ -25,13 +25,23 @@ void NetEngineLogger::Init()
 		sinks.begin(),
 		sinks.end(),
 		spdlog::thread_pool(),
-		spdlog::async_overflow_policy::overrun_oldest // 서버 멈춤 방지
+		spdlog::async_overflow_policy::block // 서버 멈춤 방지
 	);
+
+#ifdef _DEBUG
+	mpLogger->set_level(spdlog::level::trace);
+#else
+	mpLogger->set_level(spdlog::level::info);
+#endif
+
+	mpLogger->flush_on(spdlog::level::err);
 
 	mpLogger->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%t] [%s:%#] [%^%l%$] %v");
 
 	spdlog::register_logger(mpLogger);
 	spdlog::set_default_logger(mpLogger);
+
+	spdlog::flush_every(std::chrono::seconds(3));
 }
 
 std::shared_ptr<spdlog::logger> NetEngineLogger::mpLogger = nullptr;
